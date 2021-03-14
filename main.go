56c661263a63e58c85dc33bd5db0247734ad9693
Tamás Gulácsi <tamas@gulacsi.eu>
2021-03-14 13:25:50 +0100

    Store everything in memory before replacing

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"mime"
 	"net"
 	"net/http"
 	"net/http/cookiejar"
@@ -44,6 +45,8 @@ func Main() error {
 	if err != nil {
 		return fmt.Errorf("%q: %w", flag.Arg(0), err)
 	}
+	projectPath := filepath.Join(u.Host, filepath.FromSlash(path.Dir(u.Path)))
+	os.MkdirAll(projectPath, 0755)
 	var client http.Client
 	if client.Jar, err = cookiejar.New(&cookiejar.Options{}); err != nil {
 		return err
@@ -59,16 +62,14 @@ func Main() error {
 		host = u.Host
 	}
 	log.Println("allowed domains:", host)
-	c := colly.NewCollector(colly.AllowedDomains(host), colly.MaxDepth(3))
+	c := colly.NewCollector(colly.AllowedDomains(host),
+		colly.MaxDepth(2), colly.AllowURLRevisit(), colly.Async(false))
 	c.SetClient(&client)
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting: ", r.URL.String())
-	})
 
 	var mu sync.RWMutex
 	var max int
-	c.OnHTML(`a[href]`, func(e *colly.HTMLElement) {
+	maxer := c.Clone()
+	maxer.OnHTML(`a[href]`, func(e *colly.HTMLElement) {
 		foundURL := e.Request.AbsoluteURL(e.Attr("href"))
 		if i := strings.Index(foundURL, "/view.php?id="); i >= 0 {
 			if i, err := strconv.Atoi(foundURL[i+13:]); err != nil {
@@ -87,73 +88,133 @@ func Main() error {
 			}
 		}
 	})
-	if err = c.Visit(appendPath(u, "my_view_page.php")); err != nil {
+	if err = maxer.Visit(appendPath(u, "my_view_page.php")); err != nil {
 		return err
 	}
+	maxer.Wait()
 	log.Print("max:", max)
+	vl := visitLimiter{prefix: (&url.URL{Scheme: u.Scheme, Host: u.Host, Path: path.Dir(u.Path)}).String()}
 
 	c.OnHTML(`a[href]`, func(e *colly.HTMLElement) {
-		foundURL := e.Request.AbsoluteURL(e.Attr("href"))
-		if !strings.Contains(foundURL, "delete") {
+		foundURL := e.Attr("href")
+		bn := foundURL
+		if i := strings.IndexByte(bn, '?'); i >= 0 {
+			bn = bn[:i]
+		}
+		bn = path.Base(bn)
+		if bn == "login.php" || bn == "logout_page.php" || strings.HasSuffix(bn, "delete.php") {
+			return
+		}
+		e.Request.Ctx.Put("Referer", e.Request.URL.String())
+		if vl.Allow(foundURL) {
 			e.Request.Visit(foundURL)
 		}
 	})
 
-	projectPath := host
 	c.OnHTML("link[rel='stylesheet']", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
+		if URL := e.Attr("href"); vl.Allow(URL) {
+			e.Request.Visit(URL)
+		}
 	})
 
 	// search for all script tags with src attribute -- JS
 	c.OnHTML("script[src]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("src"))
+		if URL := e.Attr("src"); vl.Allow(URL) {
+			e.Request.Visit(URL)
+		}
 	})
 
 	// serach for all img tags with src attribute -- Images
 	c.OnHTML("img[src]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("src"))
+		if URL := e.Attr("src"); vl.Allow(URL) {
+			e.Request.Visit(URL)
+		}
 	})
 
 	linkMap := make(map[string]string, 8192)
-	linkPairs := make([]string, 0, 16384)
-	var replacer *strings.Replacer
-	var pairLen int
+	htmls := make(map[string]string, 8192)
 	c.OnScraped(func(resp *colly.Response) {
 		if resp.StatusCode >= 300 || len(resp.Body) == 0 {
 			return
 		}
 		hsh := sha512.Sum512_224(resp.Body)
-		fn := filepath.Join(
-			projectPath,
-			filepath.FromSlash(path.Join(path.Dir(resp.Request.URL.Path),
-				base64.URLEncoding.EncodeToString(hsh[:]),
-			)))
-		s := resp.Request.URL.String()
+		u := resp.Request.URL
+		fn := getFilename(u, resp.Headers, hsh[:])
+		s := u.String()
 		mu.Lock()
 		linkMap[s] = fn
-		linkPairs = append(linkPairs, `="`+s+`"`, `="`+fn+`"`)
 		mu.Unlock()
-		os.MkdirAll(filepath.Dir(fn), 0755)
 
-		mu.RLock()
-		if replacer == nil || pairLen != len(linkPairs) {
-			replacer = strings.NewReplacer(linkPairs...)
-			pairLen = len(linkPairs)
+		if strings.HasSuffix(fn, ".htm") || strings.HasSuffix(fn, ".html") {
+			mu.Lock()
+			htmls[s] = string(resp.Body)
+			mu.Unlock()
+		} else {
+			fn = filepath.Join(projectPath, fn)
+			log.Println(u.String(), "->", fn)
+			renameio.WriteFile(fn, resp.Body, 0444)
 		}
-		var buf bytes.Buffer
-		buf.Grow(len(resp.Body))
-		replacer.WriteString(&buf, string(resp.Body))
-		mu.RUnlock()
-		renameio.WriteFile(fn, buf.Bytes(), 0444)
 	})
 
+	index := make([]string, 0, max)
 	for i := 1; i <= max; i++ {
-		if err := c.Visit(appendPath(u, "view.php") + "?id=" + strconv.Itoa(i)); err != nil {
+		path := appendPath(u, "view.php") + "?id=" + strconv.Itoa(i)
+		if err := c.Visit(path); err != nil {
 			return fmt.Errorf("visit %q: %w", u.String(), err)
 		}
+		index = append(index, path)
 	}
+
 	c.Wait()
-	return nil
+
+	linkPairs := make([]string, 0, len(linkMap)*2)
+	for s, fn := range linkMap {
+		bn := s
+		if i := strings.LastIndexByte(s, '/'); i >= 0 {
+			bn = s[i+1:]
+		}
+		linkPairs = append(linkPairs,
+			`="`+s+`"`, `="./`+fn+`"`,
+			`="`+strings.ReplaceAll(bn, "&", "&amp;")+`"`, `="./`+fn+`"`,
+		)
+	}
+	replacer := strings.NewReplacer(linkPairs...)
+	for s, b := range htmls {
+		fh, err := os.Create(filepath.Join(projectPath, linkMap[s]))
+		if err != nil {
+			return err
+		}
+		replacer.WriteString(fh, b)
+		if err = fh.Close(); err != nil {
+			return err
+		}
+	}
+
+	fh, err := os.Create(filepath.Join(projectPath, "index.html"))
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+	log.Printf("Writing %q...", fh.Name())
+	bw := bufio.NewWriter(fh)
+	fmt.Fprintf(bw, `<!DOCTYPE html>
+<html><head><title>%s</title><head><body><p>
+`, projectPath)
+	for i, path := range index {
+		if i%100 == 0 {
+			bw.WriteString("\n</p><p>\n")
+		}
+		link := linkMap[path]
+		if i := strings.LastIndexByte(path, '='); i >= 0 {
+			path = path[i+1:]
+		}
+		fmt.Fprintf(bw, "<a href=\"%s\">%s</a>\n", link, path)
+	}
+	bw.WriteString("\n</p></body></html>")
+	if err = bw.Flush(); err != nil {
+		return err
+	}
+	return fh.Close()
 }
 
 func appendPath(u *url.URL, plusPath string) string {
@@ -188,4 +249,60 @@ func (tr contextInjectingTransport) RoundTrip(req *http.Request) (*http.Response
 		return nil, err
 	}
 	return tr.tr.RoundTrip(req.WithContext(tr.ctx))
+}
+
+func getFilename(u *url.URL, headers *http.Header, hsh []byte) string {
+	var ext string
+	if headers != nil {
+		_, params, err := mime.ParseMediaType(headers.Get("Content-Disposition"))
+		if fn, ok := params["filename"]; ok && err == nil {
+			ext = path.Ext(fn)
+		} else {
+			if ct, _, _ := mime.ParseMediaType(headers.Get("Content-Type")); ct != "" {
+				exts, _ := mime.ExtensionsByType(ct)
+				if len(exts) != 0 {
+					ext = exts[0]
+				}
+			}
+		}
+
+	}
+	if ext == "" {
+		ext = path.Ext(u.Path)
+	}
+	return base64.URLEncoding.EncodeToString(hsh) + ext
+}
+
+type visitLimiter struct {
+	mu     sync.RWMutex
+	seen   map[string]struct{}
+	prefix string
+}
+
+func (vl *visitLimiter) Allow(URL string) bool {
+	if URL[0] == '#' || URL[len(URL)-1] == '/' || strings.IndexByte(URL, '.') < 0 ||
+		strings.HasPrefix(URL, "mailto:") || strings.Contains(URL, "my_view_page.php") ||
+		((strings.HasPrefix(URL, "http://") || strings.HasPrefix(URL, "https://")) && !strings.HasPrefix(URL, vl.prefix)) {
+		return false
+	}
+	s := URL
+	if i := strings.IndexByte(s, '#'); i >= 0 {
+		s = s[:i]
+	}
+	vl.mu.RLock()
+	_, ok := vl.seen[s]
+	vl.mu.RUnlock()
+	if ok {
+		return false
+	}
+	vl.mu.Lock()
+	defer vl.mu.Unlock()
+	if _, ok = vl.seen[s]; ok {
+		return false
+	}
+	if vl.seen == nil {
+		vl.seen = make(map[string]struct{}, 8192)
+	}
+	vl.seen[s] = struct{}{}
+	return true
 }
